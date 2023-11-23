892a620ee9e677c610f3f941a8016d05b0528cb0
michele <michelesantagiuliana@hotmail.it>
2023-11-23 11:37:16 +0000

    initial commit

---
+++ components/BellIcon.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const BellIcon = () => {
+    return (
+        <div>
+            {/* Unicode character for bell: 🔔 */}
+            <span role="img" aria-label="bell">
+                🔔
+            </span>
+        </div>
+    )
+}
+
+export default BellIcon
