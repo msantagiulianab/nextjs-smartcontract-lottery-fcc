7850a590b4feb6e71602828cf5060478adad992b
michele <michelesantagiuliana@hotmail.it>
2023-11-24 12:46:19 +0000

    added dot

--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -107,6 +107,7 @@ export default function LotteryEntrance() {
                     <div>Entrance Fee: {ethers.utils.formatUnits(entranceFee, "ether")} ETH</div>
                     <div>Number of Players: {numPlayers}</div>
                     <div>Recent Winner: {recentWinner}</div>
+                    <div>WHATS UP</div>
                 </div>
             ) : (
                 <div>No Raffle Address Detected</div>
